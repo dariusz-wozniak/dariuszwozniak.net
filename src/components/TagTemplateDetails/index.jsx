b33346d3982ec841a50168ed6fec6175a94fc61b
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2019-02-02 15:39:30 +0100

    translate to pl

--- src/components/TagTemplateDetails/index.jsx
+++ src/components/TagTemplateDetails/index.jsx
@@ -15,9 +15,8 @@ class TagTemplateDetails extends React.Component {
         <div className="content__inner">
           <div className="page">
             <h1 className="page__title">
-              All Posts tagged as &quot;
-              {tagTitle}
-              &quot;
+              Tag:
+              &quot;{tagTitle}&quot;
             </h1>
             <div className="page__body">{items}</div>
           </div>
