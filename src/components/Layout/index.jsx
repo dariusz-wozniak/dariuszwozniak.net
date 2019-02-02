99a60fc2d06bc05357ea04f7d2e71395586ad519
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2019-02-02 13:38:05 +0100

    update info

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -8,7 +8,7 @@ class Layout extends React.Component {
 
     return (
       <div className="layout">
-        <Helmet defaultTitle="Blog by John Doe" />
+        <Helmet defaultTitle="Dariusz Woźniak — Blog" />
         {children}
       </div>
     )
