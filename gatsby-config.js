1f47a37f47277268c2d300f89e9fde1fa5c1c901
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2019-02-02 17:08:46 +0100

    config: setup google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -135,7 +135,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-google-analytics',
-      options: { trackingId: 'UA-73379983-2' },
+      options: { trackingId: 'UA-133706665-1' },
     },
     {
       resolve: 'gatsby-plugin-google-fonts',
