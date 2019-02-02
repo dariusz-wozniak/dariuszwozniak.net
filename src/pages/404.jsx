55acd21e266abed6ec5eeee9b6858abb9fe1d1b0
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2019-02-02 13:38:28 +0100

    translate to pl

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -12,10 +12,10 @@ class NotFoundRoute extends React.Component {
           <div className="content">
             <div className="content__inner">
               <div className="page">
-                <h1 className="page__title">NOT FOUND</h1>
+                <h1 className="page__title">404</h1>
                 <div className="page__body">
                   <p>
-                    You just hit a route that doesn&#39;t exist... the sadness.
+                    Ups... nic tu nie ma 😉
                   </p>
                 </div>
               </div>
