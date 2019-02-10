58b73dd01986514060c22eed35d3cca77e1ef5f1
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2019-02-10 23:25:56 +0100

    Revert "config: cache settings in toml file"
    This reverts commit 7cdf356afc564f455862032deb8948eaaa920f4e.

--- src/templates/post-template.jsx
+++ src/templates/post-template.jsx
@@ -17,6 +17,9 @@ class PostTemplate extends React.Component {
           <Helmet>
             <title>{`${postTitle} - ${title}`}</title>
             <meta name="description" content={description} />
+            <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+            <meta httpEquiv="Pragma" content="no-cache" />
+            <meta httpEquiv="Expires" content="0" />
           </Helmet>
           <PostTemplateDetails {...this.props} />
         </div>
