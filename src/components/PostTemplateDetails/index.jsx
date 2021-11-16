7b6c8268485c7a23c03dac7f32679828443ddd1b
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2021-11-16 18:20:29 +0100

    style: add emoji to back link

--- src/components/PostTemplateDetails/index.jsx
+++ src/components/PostTemplateDetails/index.jsx
@@ -14,7 +14,7 @@ class PostTemplateDetails extends React.Component {
     const homeBlock = (
       <div>
         <Link className="post-single__home-button" to="/">
-         Powrót
+         👈 Powrót
         </Link>
       </div>
     )
